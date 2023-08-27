337ee5faf06b55beb2cc83a77498996c74a98e1c
Dulon <dulonmolla88@gmail.com>
2023-08-28 00:16:46 +0600

    add new blog page in the portfolio UI

--- src/components/Outlates/blog/Blog.jsx
+++ src/components/Outlates/blog/Blog.jsx
@@ -1,11 +1,73 @@
+import SingleBlog from './singleBlog/SingleBlog';
+
 const Blog = () => {
   return (
-    <div className='flex justify-center items-center w-[100%] h-screen m-auto bg-slate-900'>
-      <p>Blog Page</p>
-
-      {/* You can open the modal using ID.showModal() method */}
-      <button>open modal</button>
-    </div>
+    <section className='flex justify-center items-center w-[100%] h-auto m-auto bg-slate-900'>
+      <div className='bg-slate-900 pt-5 pr-0 pb-5 pl-0'>
+        <div
+          className='w-full bg-slate-900 pt-4 pr-5 pb-6 pl-5 mt-0 mr-auto mb-0 ml-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16
+      max-w-7xl'
+        >
+          <div className='flex flex-col bg-slate-800 rounded-md p-5 text-white items-center sm:px-5 md:flex-row'>
+            <div className='flex flex-col items-start justify-center w-full h-full pt-6 pr-0 pb-6 pl-0 mb-6 md:mb-0 md:w-1/2'>
+              <div
+                className='flex flex-col items-start justify-center h-full space-y-3 transform md:pr-10 lg:pr-16
+            md:space-y-5'
+              >
+                <div
+                  className='bg-green-500 flex items-center leading-none rounded-full text-gray-50 pt-1.5 pr-3 pb-1.5 pl-2
+              uppercase inlineBlock'
+                >
+                  <p className='inline'>
+                    <svg
+                      className='w-3.5 h-3.5 mr-1'
+                      fill='currentColor'
+                      viewBox='0 0 20 20'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0
+                  00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755
+                  1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1
+                  0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'
+                      />
+                    </svg>
+                  </p>
+                  <p className='inline text-xs font-medium'>New</p>
+                </div>
+                <a className='text-4xl font-bold leading-none lg:text-5xl xl:text-6xl'>
+                  Latest Blog Here means today
+                </a>
+                <div className='pt-2 pr-0 pb-0 pl-0'>
+                  <p className='text-sm font-medium inline'>author:</p>
+                  <a className='inline text-sm font-medium mt-0 mr-1 mb-0 ml-1 underline'>
+                    Dulon Mahadi Molla
+                  </a>
+                  <p className='inline text-sm font-medium mt-0 mr-1 mb-0 ml-1'>
+                    · 23rd, April 2023 ·
+                  </p>
+                  <p className='text-gray-200 text-sm font-medium inline mt-0 mr-1 mb-0 ml-1'>
+                    1hr 20min. read
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='w-full md:w-1/2'>
+              <div className='block'>
+                <img
+                  src='https://blog.openreplay.com/images/integrating-axios-with-react-hooks/images/hero.png'
+                  className='object-cover rounded-lg max-h-64 sm:max-h-96 btn- w-full h-full'
+                />
+              </div>
+            </div>
+          </div>
+          <div className='grid grid-cols-12 sm:px-5 gap-x-8 gap-y-16'>
+            {/*todo: here all blogs map here */}
+            <SingleBlog></SingleBlog>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
---
+++ src/components/Outlates/blog/singleBlog/SingleBlog.jsx
@@ -0,0 +1,39 @@
+const SingleBlog = () => {
+  return (
+    <>
+      <div className='flex flex-col rounded-md items-start bg-slate-800 text-white p-5 col-span-12 space-y-3 sm:col-span-6 xl:col-span-4'>
+        <img
+          src='https://hashnode.com/utility/r?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1613283743682%2F5wWjMdq3O.png%3Fw%3D1200%26h%3D630%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp%26fm%3Dpng'
+          className='object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-'
+        />
+        <p
+          className='bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
+            rounded-full uppercase inlineBlock'
+        >
+          SUMMER CAMP
+        </p>
+        <a className='text-lg font-bold sm:text-xl md:text-2xl'>
+          Improving your day to the MAX
+        </a>
+        <p className='text-sm text-white'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam,
+        </p>
+        <div className='pt-2 pr-0 pb-0 pl-0'>
+          <a className='inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline'>
+            Jack Sparrow
+          </a>
+          <p className='inline text-xs font-medium mt-0 mr-1 mb-0 ml-1'>
+            · 23rd, March 2021 ·
+          </p>
+          <p className='inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1'>
+            1hr 20min. read
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleBlog;
