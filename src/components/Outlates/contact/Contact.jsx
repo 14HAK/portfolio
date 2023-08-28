241d88367ba370306a60ad1c005ba7297983b025
Dulon <dulonmolla88@gmail.com>
2023-08-28 14:36:22 +0600

    form reset when form work success

--- src/components/Outlates/contact/Contact.jsx
+++ src/components/Outlates/contact/Contact.jsx
@@ -17,6 +17,7 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
+          e.target.reset();
         },
         (error) => {
           console.log(error.text);
